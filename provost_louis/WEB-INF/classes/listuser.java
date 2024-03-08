8f6fecdb707ce2e41f02033fa769927dca438e19
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-03-08 12:20:53 +0100

    Update listuser.java

--- provost_louis/WEB-INF/classes/listuser.java
+++ provost_louis/WEB-INF/classes/listuser.java
@@ -12,66 +12,39 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/ListeUtilisateursServlet")
-public class ListeUtilisateursServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+@WebServlet("/listuser")
+public class listuser extends HttpServlet {
+  final String URL = "jdbc:mysql://127.0.0.1/tomcat";
+  final String USER = "tomcat";
+  final String PWD = "password";
+  final String DRIVER = "com.mysql.cj.jdbc.Driver";
+  Connection conn = null;
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-
-        PrintWriter out = response.getWriter();
-        out.println("<html><head><title>Liste des utilisateurs</title></head><body>");
-
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        ResultSet rs = null;
-
-        try {
-            // Charger le driver JDBC
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            // Établir la connexion à la base de données
-            String url = "jdbc:mysql://localhost:3306/tomcat";
-            String utilisateur = "tomcat";
-            String motDePasse = "louis";
-            conn = DriverManager.getConnection(URL , USER , PWD);
-
-            // Exécuter la requête SQL
-            String sql = "SELECT * FROM users";
-            stmt = conn.prepareStatement(sql);
-            rs = stmt.executeQuery();
-
-            // Afficher les résultats
-            out.println("<h2>Liste des utilisateurs</h2><ul>");
-            while (rs.next()) {
-                String nom = rs.getString("lastname");
-                String prenom = rs.getString("firstname");
-                out.println("<li>" + prenom + " " + nom + "</li>");
-            }
-            out.println("</ul>");
-
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            out.println("Erreur : " + e.getMessage());
-
-        } finally {
-            // Fermer les ressources
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-                if (stmt != null) {
-                    stmt.close();
-                }
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-
-        out.println("</body></html>");
+  public void init() throws ServletException {
+    try {
+      Class.forName(DRIVER);
+      conn = DriverManager.getConnection(URL, USER, PWD);
+    } catch (ClassNotFoundException | SQLException e) {
+      e.printStackTrace();
     }
+  }
+
+public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  try {
+    Statement stmt = conn.createStatement();
+    ResultSet results = stmt.executeQuery("SQL query");
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    ResultSetMetaData rsmd = results.getMetaData();
+    int nbCols = rsmd.getColumnCount();
+    while (results.next()) {
+      for (int i = 1; i <= nbCols; i++) {
+        out.println(results.getString(i);
+      }
+    }
+    results.close();
+    out.close();
+  } catch (SQLException e) {
+    e.printStackTrace();
+  }
 }
