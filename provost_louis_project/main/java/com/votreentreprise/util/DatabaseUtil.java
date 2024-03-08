12881486e539c26450f7897b46c95c3fc24adffd
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-03-08 14:22:45 +0100

    Create DatabaseUtil.java

---
+++ provost_louis_project/main/java/com/votreentreprise/util/DatabaseUtil.java
@@ -0,0 +1,7 @@
+package com.votreentreprise.util;
+
+// Importez les packages nécessaires
+
+public class DatabaseUtil {
+    // Méthodes pour établir la connexion à la base de données, ajouter un utilisateur, obtenir la liste des utilisateurs, etc.
+}
