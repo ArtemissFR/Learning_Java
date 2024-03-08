4c322d166d328410c498edc4fdf62e24e0b7a362
Artemiss <154614337+ArtemissFR@users.noreply.github.com>
2024-03-08 14:23:29 +0100

    Create User.java

---
+++ provost_louis_project/main/java/com/votreentreprise/models/User.java
@@ -0,0 +1,9 @@
+package com.votreentreprise.models;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String password;
+
+    // Constructeurs, getters, setters
+}
